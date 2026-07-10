ad0ad34ef0bfb77f757ccf543662169f855c190c
Bhakti Kavathiya <bhaktikavathiya5@gmail.com>
2026-07-10 18:29:44 +0530

    Guess the Number v1.1: Improve game and add .gitignore

--- 01-guess-the-number/main.c
+++ 01-guess-the-number/main.c
@@ -9,24 +9,24 @@ int main(){
     printf("\n");
     printf("Instructions:\n"
         "1. In this game you guess a random number between 1 to 100.\n"
-        "2. If you guess it correct you win\n"
+        "2. If you guess it correctly, you win\n"
         "3. If your guess is incorrect,the game will tell you whether the correct number is higher or lower.\n");
     
     int num = rand() % 100 + 1;
 
     int guess;
-    printf("\nEnter your guess(1-100):");
+    printf("\nEnter your guess(1-100): ");
     scanf("%d", &guess);
     
     printf("\n");
 
     if (num == guess){
-        printf("\nCorrect guess! You won!!");
+        printf("\nCongratulations! You Won!!");
     } else if (num>guess){
-        printf("\nOOPS!! Wrong Guess\n");
+        printf("\nNot quite\n");
         printf("The correct number is higher than your guess.\n");
     } else {
-        printf("\nOh no!! Wrong Guess\n");
+        printf("\nGood try!d\n");
         printf("The correct number is lower than your guess.\n");
     }
 
